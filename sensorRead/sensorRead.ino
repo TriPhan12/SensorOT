31dc07b4731cf3c1ca027de4b5fea6e9fe6344da
TriPhan12 <pmtri96@gmail.com>
2019-03-23 21:06:13 +0700

    raw packet send (only cli command)

--- sensorRead/sensorRead.ino
+++ sensorRead/sensorRead.ino
@@ -1,4 +1,3 @@
-
 void setup()
 {
   Serial.begin(115200);
@@ -6,31 +5,37 @@ void setup()
 
 void receivePacket();
 void sendPacket();
-static void formatPrint();
+void formatPrint();
+//char getData(*char packet);
 
 void loop()
 {
-
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i <= 2; i++)
   {
-    sendPacket()
+    sendPacket("fdde:ad00:beef::","temp1");
+    delay(100);
   }
+  
 }
 
 void readTemp()
 {
 }
 
-void receivePacket(char packetReceived[10])
-{
+void receivePacket(char *packet)
+{    
+  //char receiveData = getData(*packet);
+
 }
 
 void sendPacket(char *destIpv6Addr, char *message)
 {
   //How-to-use
   //sendPacket("fdde:ad00:....","message");
-  Serial.print("udp send")
+  Serial.print("udp send ");
   Serial.print(destIpv6Addr);
   Serial.print(" 1212 ");           //print the UDP port, this case uses port 1212
-  Serial.println(message);
+  Serial.print(message);  
+  Serial.write(0x03);             //0x03 a.k.a "End of text" in UTF-8
+  Serial.println();
 }
