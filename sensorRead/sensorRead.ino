d988e47a12ae93fc7de184b840dcbdc6fc29852b
TriPhan12 <pmtri96@gmail.com>
2019-03-26 08:56:39 +0700

    developing receivePacket

--- sensorRead/sensorRead.ino
+++ sensorRead/sensorRead.ino
@@ -1,11 +1,11 @@
+#include <string.h>
 void setup()
 {
   Serial.begin(115200);
 }
 //Global variable
-const int receivePacketInfo = 2; //3 parameters: bytes received; source address; message content (start from 0)
-String receiveMessage[receivePacketInfo] = {};
-int node = 0; //The number of node in Thread network (start from 0)
+String sourceAddress = "", receiveMessage = "";
+int receiveByte = 0;
 //Functions
 String receivePacket();
 void sendPacket();
@@ -18,58 +18,74 @@ void loop()
   {
     //wait for serial port to connect.
   }
-  //String mess = receivePacket();
-  Serial.print("Day la message: ");
-  //Serial.println(receiveMessage[3]);
-  delay(1000);
+  Serial.println("Day la so byte: ");
+  Serial.println(receivePacket("2 bytes from fdde:ad00:beef:0:cf3c:df09:f013:55a1 14152 Hello,World!", 1));
+  Serial.println("Day la source address: ");
+  Serial.println(receivePacket("2 bytes from fdde:ad00:beef:0:cf3c:df09:f013:55a1 14152 Hello,World!", 2));
+  Serial.println("Day la message: ");
+  Serial.println(receivePacket("2 bytes from fdde:ad00:beef:0:cf3c:df09:f013:55a1 14152 Hello,World!", 3));
+
+  while (1)
+  {
+    delay(1000);
+  }
 }
 
-void readTemp()
-{
-}
-
-void receivePacket(char *packet)
+String receivePacket(char *packet, int data)
 {
   /*How-to-use
   receivePacket(packet);
   Examble:
-  sendPacket("2 bytes from fdde:ad00:beef:0:cf3c:df09:f013:55a1 14152 Hello,World!");
-              ! -> pointer = 0
-                           ! -> pointer = 3
-                                                                      ! -> pointer = 5
+  receivePacket("2 bytes from fdde:ad00:beef:0:cf3c:df09:f013:55a1 14152 Hello,World!", 1);
+                 ! -> pointer = 0                                                       ! 1 for data at pointer 0
+                              ! -> pointer = 3                                          ! 2 for data at pointer 3
+                                                                         ! -> pointer = 5|3 for data at pointer 5
   */
-  String stringPacket = String(*packet);
   int pointer = 0;
-  for (int i = 0; i <= stringPacket.length(); i++)
+  String tampont = "";
+  int i = 0;
+  switch (data)
   {
-    String buffer = "";
-    if (stringPacket[i] != " ")
-    {
-      buffer += stringPacket[i]; //when not reach " " symbol add char to buffer
-    }
-    else if (pointer == 0)
-    {
-      receiveMessage[0] = buffer; //assign buffer to byte number parameter
-    }
-    else if (pointer == 3)
-    {
-      receiveMessage[1] = buffer; //assign buffer to address parameter
-    }
-    else if (pointer == 5)
-    {
-      receiveMessage[2] = buffer; //assign buffer to message parameter
-    }
-    pointer++; //increase pointer to the beginning of a word
+  case 1:
+    pointer = 0;
+    break;
+  case 2:
+    //Serial.println(data);
+    pointer = 3;
+    break;
+  case 3:
+    //Serial.println(data);
+    pointer = 5;
+    break;
+  default:
+    break;
   }
+  while (pointer >= 0)
+  {
+    if ((pointer != 0)|| (packet[i] == ' '))
+    {
+      pointer = pointer - 1;
+    }
+    else if ((pointer == 0) || (packet[i] != ' '))
+    {
+      tampont = tampont + packet[i]; //when not reach " " symbol add char to buffer
+      //Serial.println(tampont);      
+    }
+    else if ((pointer == 0) || (packet[i] == ' ')){
+      pointer = pointer -1;
+    }  
+    i = i + 1;
+  }
+  return tampont;
 }
 
 void sendPacket(char *destIpv6Addr, char *message)
 {
   /*How-to-use
-  sendPacket("address in IPv6","message");
-  Examble:
-  sendPacket("fdde:ad00:beef::....","Hello,World!");
-  */
+sendPacket("address in IPv6","message");
+Examble:
+sendPacket("fdde:ad00:beef::....","Hello,World!");
+*/
   Serial.print("udp send ");
   Serial.print(destIpv6Addr);
   Serial.print(" 1212 "); //print the UDP port, this case uses port 1212
