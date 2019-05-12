1947cc074a671513e0fe567e38a7e486b3c9e90b
TriPhan12 <pmtri96@gmail.com>
2019-05-12 15:44:23 +0700

    Code for raspberry up load sensor data to MQTT

---
+++ sendToMQTT.py
@@ -0,0 +1,15 @@
+
+### Test code read serial
+# import serial
+# ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+# while True:
+#     data = ser.readline()
+#     if data:
+#         print(data)
+
+#####
+
+
+####Read data over usb from Arduino to RaspberryPi, cleans it up and publishes to an MQTT broker
+#Reference: https://codereview.stackexchange.com/questions/146914/script-that-reads-data-over-usb-from-arduino-to-raspberrypi-cleans-it-up-and-pu
