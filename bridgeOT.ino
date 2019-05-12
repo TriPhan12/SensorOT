5d51fc72b205b3b4bd03e3f53089a5e6cdabf0af
TriPhan12 <pmtri96@gmail.com>
2019-05-12 15:45:40 +0700

    receive data from cc2538 cli send to Raspberry via USB

---
+++ bridgeOT.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+
+// software serial #1: RX = digital pin 10, TX = digital pin 11
+SoftwareSerial portOne(10, 11);
+
+void setup
+{
+  // Start software serial port
+  Serial.begin(9600);  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  portOne.begin(9600);
+}
+
+void loop(){
+  portOne.listen();
+  while (portOne.available() > 0) {        
+    char inByte = portOne.read();
+    String inputString = "";
+    while(InByte != 0x03){
+      inputString += inByte;
+    }    
+    Serial.println(inputString);
+  }
+}
